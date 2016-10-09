087348a265dd929e6c184ccfbc989e9d320a5cfe
Micah Hummert <mhummert@liberty.edu>
2016-10-09 14:21:34 -0400

    Initial Commit

---
+++ Comic Cleaner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Comic Cleaner
+//
+//  Created by Micah Hummert on 10/9/16.
+//  Copyright © 2016 Hummert IT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
